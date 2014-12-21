a2069cd5fdbded440e3524609bc2db41310e4d16
ptrv <mail@petervasil.net>
2014-12-21 01:13:09 +0100

    Update SSH_AUTH_SOCK paths

--- custom/conf_alderaan.zsh
+++ custom/conf_alderaan.zsh
@@ -1,4 +1,5 @@
-SSH_AUTH_SOCK=`netstat -xl | grep -o '/run/user/1001/keyring-.*/ssh$'`
+# SSH_AUTH_SOCK=`netstat -xl | grep -o '/run/user/1001/keyring-.*/ssh$'`
+SSH_AUTH_SOCK=/run/user/1001/keyring/ssh
 [ -z "$SSH_AUTH_SOCK" ] || export SSH_AUTH_SOCK
 # GPG_AGENT_INFO=`netstat -xl | grep -o '/home/peter/.cache/keyring-.*/gpg$'`
 # [ -z "$GPG_AGENT_INFO" ] || export GPG_AGENT_INFO=$GPG_AGENT_INFO:0:1
--- custom/conf_anoth.zsh
+++ custom/conf_anoth.zsh
@@ -1,2 +1,3 @@
-SSH_AUTH_SOCK=`netstat -xl | grep -o '/run/user/1001/keyring-.*/ssh$'`
+# SSH_AUTH_SOCK=`netstat -xl | grep -o '/run/user/1001/keyring-.*/ssh$'`
+SSH_AUTH_SOCK=/run/user/1001/keyring/ssh
 [ -z "$SSH_AUTH_SOCK" ] || export SSH_AUTH_SOCK
