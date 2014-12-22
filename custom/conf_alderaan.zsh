e19fceccc4b760803bf15537801d9a0e5a7391cb
ptrv <mail@petervasil.net>
2014-12-22 11:45:33 +0100

    Remove go completion

--- custom/conf_alderaan.zsh
+++ custom/conf_alderaan.zsh
@@ -3,6 +3,3 @@ SSH_AUTH_SOCK=/run/user/1001/keyring/ssh
 [ -z "$SSH_AUTH_SOCK" ] || export SSH_AUTH_SOCK
 # GPG_AGENT_INFO=`netstat -xl | grep -o '/home/peter/.cache/keyring-.*/gpg$'`
 # [ -z "$GPG_AGENT_INFO" ] || export GPG_AGENT_INFO=$GPG_AGENT_INFO:0:1
-
-# go completion
-[[ -f "/usr/share/go/misc/zsh/go" ]] && source "/usr/share/go/misc/zsh/go"
