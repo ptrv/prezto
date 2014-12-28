8c0c10c97057cd5c9db77627806284012e67a6a3
ptrv <mail@petervasil.net>
2014-12-28 19:17:47 +0100

    Enable svn for zsh vcs_info

--- custom/conf_alderaan.zsh
+++ custom/conf_alderaan.zsh
@@ -3,3 +3,5 @@ SSH_AUTH_SOCK=/run/user/1001/keyring/ssh
 [ -z "$SSH_AUTH_SOCK" ] || export SSH_AUTH_SOCK
 # GPG_AGENT_INFO=`netstat -xl | grep -o '/home/peter/.cache/keyring-.*/gpg$'`
 # [ -z "$GPG_AGENT_INFO" ] || export GPG_AGENT_INFO=$GPG_AGENT_INFO:0:1
+
+zstyle ':vcs_info:*' enable git svn
