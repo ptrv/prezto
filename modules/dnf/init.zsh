970752b7dd9a6fd5405b993d820cc14aaeb88afb
ptrv <mail@petervasil.net>
2017-01-29 17:06:43 +0100

    Update dnf module
    Remove alias to deprecated dnf update and make it pointing to dnf upgrade.
    Add sudo for dnf history.

--- modules/dnf/init.zsh
+++ modules/dnf/init.zsh
@@ -16,13 +16,12 @@ fi
 #
 
 alias dnfc='sudo dnf clean all'    # Cleans the cache.
-alias dnfh='dnf history'           # Displays history.
+alias dnfh='sudo dnf history'           # Displays history.
 alias dnfi='sudo dnf install'      # Installs package(s).
 alias dnfl='dnf list'              # Lists packages.
 alias dnfL='dnf list installed'    # Lists installed packages.
 alias dnfq='dnf info'              # Displays package information.
 alias dnfr='sudo dnf remove'       # Removes package(s).
 alias dnfs='dnf search'            # Searches for a package.
-alias dnfu='sudo dnf update'       # Updates packages.
-alias dnfU='sudo dnf upgrade'      # Upgrades packages.
+alias dnfu='sudo dnf upgrade'      # Updates packages.
 alias dnfls='dnf repoquery -l'
